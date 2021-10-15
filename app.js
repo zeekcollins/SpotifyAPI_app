10ae2d08439bf2682f11d69d2ade115eb4428ae3
Zeek Collins <collezek@gmail.com>
2021-10-15 03:05:07 -0500

    testing

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ window.onload = () => {
     const spotUri = "https://api.spotify.com/v1/search";
 
     const getAccessToken = (key) => {
-        let split_str = (window.location.href.length > 0) ? window.location.href.split("#") : [];
-        let urlVariables = (split_str != undefined && split_str.length > 1 && split_str[1].length > 0) ? split_str[1].split("&") : [];
-        let param;
+      let sPageURL = window.location.search.substring(1);
+      let urlVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [];
+      let param;
 
-        for (let i = 0; i < urlVariables.length; i++) {
-            param = urlVariables[i].split("=");
-            if (param[0] === key) {
-                return param[1];
-                // return (param[1] === undefined) ? true : decodeURIComponent(param[1]);
-            }
-        }
+      let split_str = (window.location.href.length > 0) ? window.location.href.split("#") : [];
+      urlVariables = (split_str != undefined && split_str.length > 1 && split_str[1].length > 0) ? split_str[1].split("&") : [];
+
+      for (let i = 0; i < urlVariables.length; i++) {
+          param = urlVariables[i].split("=");
+          if (param[0] === key) {
+              return param[1];
+              // return (param[1] === undefined) ? true : decodeURIComponent(param[1]);
+          }
+      }
     }
 
     const accessToken = getAccessToken('access_token');
     const client_id = "7bc91a436e9c4482bc405fb3bfa9b9f4";
     // const redirect_uri = "http%3A%2F%2Flocalhost%3A5500%2Findex.html";
+    let now = 305;
     const redirect_uri = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
 
     const get = `${spotAuth}?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
