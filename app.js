c2524f02cdde39f109962beb1100175e3bd83ad1
Zeek Collins <collezek@gmail.com>
2021-10-14 23:07:52 -0500

    edit geturlparameter

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 window.onload = () => {
-    const xhttp = new XMLHttpRequest;
     const spotAuth = "https://accounts.spotify.com/authorize";
     const spotUri = "https://api.spotify.com/v1/search";
 
-    const getAccessToken = (key) => {
-        let pageUrl = window.location.search.substring(1);
-        let urlVariables = (pageUrl != undefined && pageUrl.length > 0) ? pageUrl.split('#') : [];
-        let param;
-        let i;
-
-        let split_str = (window.location.href.length > 0) ? window.location.href.split("#") : [];
-        urlVariables = (split_str != undefined && split_str.length > 1 && split_str[1].length > 0) ? split_str[1].split("&") : [];
-        
-        urlVariables.forEach((item) => {
-            param = item.split("=");
-            if (param[0] == key) {
-                return (param[1] === undefined) ? true : decodeURIComponent(param[1]);
+    const getUrlParameter = (sParam) => {
+        let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
+            sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
+            sParameterName,
+            i;
+        let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
+        sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
-        });
-    }
+        }
+    };
 
-    const accessToken = getAccessToken('access_token');
-    const CLIENT_ID = "7bc91a436e9c4482bc405fb3bfa9b9f4";
-    const REDIRECT_URI = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
+    const accessToken = getUrlParameter('access_token');
+    const client_id = "7bc91a436e9c4482bc405fb3bfa9b9f4";
+    const redirect_uri = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
 
-    const get = `${spotAuth}?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`;
+    const get = `${spotAuth}?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
     if (accessToken == null || accessToken == undefined || accessToken == "") {
         window.location.replace(get);
     }
@@ -34,11 +31,11 @@ window.onload = () => {
         let rawQuery = document.querySelector(".search-text").value;
         let searchQuery = encodeURI(rawQuery);
         $.ajax({
-            url: `spotUri?=${searchQuery}&type=track`,
+            url: `${spotUri}?=${searchQuery}&type=track`,
             headers: {
                 'Authorization': 'Bearer ' + accessToken
             },
-            success: (response) => {
+            success: function(response) {
                 console.log(response);
             }
         });
