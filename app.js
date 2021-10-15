84225b98cfc73c10ade7deffd9344e815bc52263
Zeek Collins <collezek@gmail.com>
2021-10-14 22:34:42 -0500

    added functionality to call Spotify API

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 window.onload = () => {
+    const xhttp = new XMLHttpRequest;
+    const spotAuth = "https://accounts.spotify.com/authorize";
+    const spotUri = "https://api.spotify.com/v1/search";
+
     const getAccessToken = (key) => {
         let pageUrl = window.location.search.substring(1);
         let urlVariables = (pageUrl != undefined && pageUrl.length > 0) ? pageUrl.split('#') : [];
@@ -17,6 +21,27 @@ window.onload = () => {
     }
 
     const accessToken = getAccessToken('access_token');
-    const CLIENT_ID = process.env.CLIENT_ID;
-    
+    const CLIENT_ID = "7bc91a436e9c4482bc405fb3bfa9b9f4";
+    const REDIRECT_URI = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
+
+    const get = `${spotAuth}?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`;
+    if (accessToken == null || accessToken == undefined || accessToken == "") {
+        window.location.replace(get);
+    }
+
+    const searchBtn = document.querySelector(".search-btn");
+    searchBtn.addEventListener("click", () => {
+        let rawQuery = document.querySelector(".search-text").value;
+        let searchQuery = encodeURI(rawQuery);
+        $.ajax({
+            url: `spotUri?=${searchQuery}&type=track`,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            success: (response) => {
+                console.log(response);
+            }
+        });
+    });
+
 };
