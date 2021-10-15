55a21c198688f85cc7a1e243b79f46f1f59405e1
Zeek Collins <collezek@gmail.com>
2021-10-15 03:41:10 -0500

    redirect change

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ window.onload = () => {
 
     const accessToken = getAccessToken('access_token');
     const client_id = "7bc91a436e9c4482bc405fb3bfa9b9f4";
+    
+    // let now = 340;
+    const redirect_uri = "https%3A%2F%2Fspotifyapi-app.herokuapp.com";
+    // const redirect_uri = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
     // const redirect_uri = "http%3A%2F%2Flocalhost%3A5500%2Findex.html";
-    let now = 305;
-    const redirect_uri = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
 
     const get = `${spotAuth}?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
     if (accessToken == null || accessToken == undefined || accessToken == "") {
