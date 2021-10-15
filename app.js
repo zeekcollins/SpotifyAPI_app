1d705911cd60763c8d984176c1b77eeb24bcd059
Zeek Collins <collezek@gmail.com>
2021-10-15 01:29:01 -0500

    testing

--- app.js
+++ app.js
@@ -21,11 +21,8 @@ $( document ).ready(function() {
 
    // AUTHORIZE with Spotify (if needed)
    // *************** REPLACE THESE VALUES! *************************
-   let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
-   // Use the following site to convert your regular url to the encoded version:
-   // https://www.url-encode-decode.com/
-   let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
-   // *************** END *************************
+   const client_id = "7bc91a436e9c4482bc405fb3bfa9b9f4";
+    const redirect_uri = "https%3A%2F%2Fzeekcollins.github.io%2FSpotifyAPI_app";
 
    const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
    // Don't authorize if we have an access token already
