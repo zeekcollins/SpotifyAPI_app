dc63ebee4444b743672e3ab6a2d3b7bbff08a10a
Zeek Collins <zeekcollins@Zeeks-Mac-mini.hsd1.wa.comcast.net>
2024-03-27 16:48:55 -0700

    Change host to GitHub Pages

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ $(document).ready(function() {
 
   const accessToken = getAccessToken('access_token');
   const client_id = "7bc91a436e9c4482bc405fb3bfa9b9f4";
-  const redirect_uri = "https%3A%2F%2Fspotifyapi-app.herokuapp.com";
+  const redirect_uri = encodeURIComponent("https://zeekcollins.github.io/SpotifyAPI_app/");
+
 
 
   const get = `${spotAuth}?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
